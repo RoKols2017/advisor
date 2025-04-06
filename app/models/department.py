411219ea54b05653b91522afffd22a47f5b93707
RoKols2017 <RoKols2017@gmail.com>
2025-04-06 08:45:54 +0300

    Исправлен config для исползования PostgreSQL Исправлен модель для департамента check_db.py - вспомогательный код для тестирования подключению к БД

--- app/models/department.py
+++ app/models/department.py
@@ -7,7 +7,7 @@ class Department(db.Model):
     __tablename__ = "departments"
 
     id = db.Column(db.Integer, primary_key=True, index=True)
-    code = db.Column(db.String(10), unique=True, nullable=False)
+    code = db.Column(db.String(20), unique=True, nullable=False)
     name = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
