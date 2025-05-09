e26c08f81ef31fd4593fe51e4be1be615e18058e
RoKols2017 <RoKols2017@gmail.com>
2025-05-10 00:04:28 +0300

    Delete app directory

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # 🧠 загружает переменные из .env
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///advisor.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
